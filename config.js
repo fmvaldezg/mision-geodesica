347d62950f174d389f69664bea656cd379029eeb
Felipe Valdez <fmvaldezg@yahoo.com>
2026-07-07 14:50:46 -0400

    Añadir capitulo 0

--- config.js
+++ config.js
@@ -1,84 +1,39 @@
 var config = {
-    style: 'map/style.json',
-    showMarkers: true,
+    style: 'https://tiles.openfreemap.org/styles/liberty',
+    showMarkers: false,
     markerColor: '#3FB1CE',
-    inset: true, // if inset map is set to true, legend will be disabled.
-    legend: false, // if legend is set to true, inset will be disabled.
-    theme: 'dark',
-    use3dTerrain: false, //set true for enabling 3D maps. You will need to provide your own terrain tiles.
-    useCustomLayers: false, //set true for enabling custom layers from sources.js
-    bookmarks: true,
-    chapterReturn: true,
-    title: 'The Title Text of this Story',
+    inset: true,
+    legend: false,
+    theme: 'light',
+    use3dTerrain: false,
+    useCustomLayers: true,
+    bookmarks: false,
+    chapterReturn: false,
+    title: 'Medir la Tierra',
     logo: '',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
-    mobileview: '<div id="rotate-mobile"><em>For optimal viewing of this storytelling map on mobile, rotate your device to a horizontal orientation.</em><br><br><img src="images/device.png">', // to add custom messaging in the header for mobile devices
-    footer: 'Source: source citations, etc.<br> Created using <a href="https://github.com/digidem/maplibre-storymap" target="_blank">MapLibre Storytelling</a> template.',
+    subtitle: 'La Misión Geodésica Francesa al Ecuador (1736–1743)',
+    byline: 'Por Felipe Valdez',
+    footer: 'Fuentes: La Condamine (1745, 1751), Gallica BnF; Juan y Ulloa (1748), BNE. Creado con la plantilla <a href="https://github.com/digidem/maplibre-storymap" target="_blank">MapLibre Storytelling</a> y datos de <a href="https://openfreemap.org" target="_blank">OpenFreeMap</a>.',
     chapters: [
         {
-            id: 'slug-style-id',
+            id: 'cap-0-apertura',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
-            image: './path/to/image/source.png',
-            caption: 'Source image caption',
-            website: '<a href="" target="_blank">Website</a>',
-            author: 'Digital Storyteller',
-            legend: '<span style="font-size: 0.85em;"><em>Legend content</em></span>',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            title: '¿Qué forma tiene la Tierra?',
+            image: './images/cap0_esferoide_newton.jpg',
+            caption: 'Comparación de los dos modelos del esferoide terrestre, siglo XVIII. Dominio público.',
+            description: 'En 1734, la Academia Real de Ciencias de París enfrentaba una disputa que dividía a la ciencia europea: ¿es la Tierra un esferoide achatado en los polos, como sostenía Isaac Newton, o achatado en el ecuador, como defendía la escuela de Jacques Cassini? La única forma de resolverlo era medir. La Academia decidió enviar dos expediciones simultáneas a los extremos del planeta: una a Laponia, dirigida por Maupertuis (1736–1737), y otra al territorio de la Real Audiencia de Quito, en el actual Ecuador.',
             location: {
-                center: [35, 1],
-                zoom: 3.5,
+                center: [0.0, 0.0],
+                zoom: 1.6,
                 pitch: 0,
                 bearing: 0
             },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            spinGlobe: false,
-            mapInteractive: true,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-                // {
-                //      layer: 'countries-fill',
-                //      opacity: 1
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
-        {
-            id: 'other-identifier',
-            alignment: 'right',
-            hidden: false,
-            title: 'Second Title',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
-            location: {
-                center: [-79, 0],
-                zoom: 5,
-                pitch: 0,
-                bearing: -90
-            },
-            mapAnimation: 'flyTo',
+            mapAnimation: 'jumpTo',
             rotateAnimation: false,
             mapInteractive: false,
             callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'countries-fill',
-                //     opacity: 0
-                // }
-            ],
+            onChapterEnter: [],
             onChapterExit: []
         }
     ]
