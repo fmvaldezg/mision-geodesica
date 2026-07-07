ce3e432bddc50d0c3e1c40479b72b4994db539b2
Felipe Valdez <fmvaldezg@yahoo.com>
2026-07-07 14:58:29 -0400

    Se anaden los puntos y lineas personalizados en el erchivo source.js

--- sources.js
+++ sources.js
@@ -1,29 +1,57 @@
 var externalData = {
     sources: [
-//      {
-//          "name": '',
-//          "type": '',
-//          "url": ''
-//        },
+        {
+            "name": "triangulacion",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [
+                        { "type": "Feature", "properties": { "nombre": "Pichincha" },
+                          "geometry": { "type": "Point", "coordinates": [-78.5980, -0.1710] } },
+                        { "type": "Feature", "properties": { "nombre": "Chimborazo" },
+                          "geometry": { "type": "Point", "coordinates": [-78.8170, -1.4690] } },
+                        {
+                            "type": "Feature",
+                            "properties": { "nombre": "Cadena de triángulos" },
+                            "geometry": {
+                                "type": "MultiLineString",
+                                "coordinates": [
+                                    [[-78.5980, -0.1710], [-78.8170, -1.4690]]
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+        }
     ],
     layers: [
-//      {
-//          "id": "",
-//          "type": "",
-//          "source": "",
-//          "source-layer": "",
-//          "layout": {
-//              "text-field": ["to-string", ["get", "Name"]],
-//              "text-font": ["Open Sans SemiBold", "Arial Unicode MS Regular"],
-//              "text-size": 10,
-//              "text-padding": 0
-//          },
-//          "paint": {
-//              "text-color": "#ffffff",
-//              "text-halo-color": "hsl(0, 99%, 1%)",
-//              "text-halo-width": 1,
-//              "text-opacity": 0
-//          }
-//      }
+        {
+            "id": "triangulacion-lineas",
+            "type": "line",
+            "source": "triangulacion",
+            "filter": ["==", "$type", "LineString"],
+            "paint": {
+                "line-color": "#1c4587",
+                "line-width": 1.5,
+                "line-dasharray": [2, 2],
+                "line-opacity": 0
+            }
+        },
+        {
+            "id": "triangulacion-cumbres",
+            "type": "circle",
+            "source": "triangulacion",
+            "filter": ["==", "$type", "Point"],
+            "paint": {
+                "circle-radius": 6,
+                "circle-color": "#1c4587",
+                "circle-stroke-color": "#ffffff",
+                "circle-stroke-width": 2,
+                "circle-opacity": 0,
+                "circle-stroke-opacity": 0
+            }
+        }
     ]
 };
